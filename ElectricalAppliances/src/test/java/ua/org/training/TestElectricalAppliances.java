388d9f22a542f3580f1ad2dd3b092cf446992b38
kissik <karayuziv@gmail.com>
2019-12-04 00:03:42 +0200

    ElectricalAppliances. Test. Add Plug out test

--- ElectricalAppliances/src/test/java/ua/org/training/TestElectricalAppliances.java
+++ ElectricalAppliances/src/test/java/ua/org/training/TestElectricalAppliances.java
@@ -1,14 +1,28 @@
 package ua.org.training;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ua.org.training.model.ElectricalAppliances;
 
 public class TestElectricalAppliances {
+    private static  ElectricalAppliances electricalAppliances;
+
+    @BeforeClass
+    public static void testInit(){
+        electricalAppliances = new ElectricalAppliances();
+    }
+
     @Test
     public void testIsPlugIn(){
-        ElectricalAppliances electricalAppliances = new ElectricalAppliances();
+        electricalAppliances.setPlugIn(true);
         Assert.assertTrue(electricalAppliances.isPlugIn());
+    }
+
+    @Test
+    public void testIsPlugOut(){
+        electricalAppliances.setPlugIn(false);
+        Assert.assertFalse(electricalAppliances.isPlugIn());
     }
 
 
