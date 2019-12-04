21cd154195897ea5efd3e31638efdebc4380d287
kissik <karayuziv@gmail.com>
2019-12-04 15:46:04 +0200

    Dimensions. JavaDoc. Add documentation

--- ElectricalAppliances/src/main/java/ua/org/training/model/Dimensions.java
+++ ElectricalAppliances/src/main/java/ua/org/training/model/Dimensions.java
@@ -2,56 +2,118 @@ package ua.org.training.model;
 
 import ua.org.training.view.GlobalConstants;
 
+/**
+ * Dimensions is the class of such dimensions as width, height and depth of a thing
+ * @author      <a href="mailto:iryna.v.afanasieva@gmail.com">Ira Afanasieva</a>
+ * @version     1.0, 12/4/2019
+ */
 public class Dimensions {
 
     private int width;
     private int height;
     private int depth;
 
+    /**
+     * Dimensions default constructor
+     * with default values:
+     * the width field is   DIMENSION_SIZE_DEFAULT_VALUE
+     * the height field is  DIMENSION_SIZE_DEFAULT_VALUE
+     * the depth field is   DIMENSION_SIZE_DEFAULT_VALUE
+     * Please see the {@link ua.org.training.view.GlobalConstants} interface for values
+     */
     public Dimensions(){
-        width = 0;
-        height = 0;
-        depth = 0;
+        width = GlobalConstants.DIMENSION_SIZE_DEFAULT_VALUE;
+        height = GlobalConstants.DIMENSION_SIZE_DEFAULT_VALUE;
+        depth = GlobalConstants.DIMENSION_SIZE_DEFAULT_VALUE;
     }
 
+    /**
+     * Dimensions constructor with parameters
+     * @param width     integer value of the width in the centimeters
+     * @param height    integer value of the height in the centimeters
+     * @param depth     integer value of the depth in the centimeters
+     */
     public Dimensions(int width, int height, int depth){
         this.width = width;
         this.height = height;
         this.depth = depth;
     }
 
+    /**
+     * The width field getter method
+     * @return integer value of the width in the centimeters
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * The width field setter method
+     * @param width     integer value of the width in the centimeters
+     * @return          reference to the current Dimensions object
+     */
     public Dimensions setWidth(int width) {
         this.width = width;
         return this;
     }
 
+    /**
+     * The height field getter method
+     * @return          integer value of the height in the centimeters
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * The height field setter method
+     * @param height    integer value of the height in the centimeters
+     * @return          reference to the current Dimensions object
+     */
     public Dimensions setHeight(int height) {
         this.height = height;
         return this;
     }
 
+    /**
+     * The depth field getter method
+     * @return          integer value of the depth in the centimeters
+     */
     public int getDepth() {
         return depth;
     }
 
+    /**
+     * The depth field setter method
+     * @param depth     integer value of the depth in the centimeters
+     * @return          reference to the current Dimensions object
+     */
     public Dimensions setDepth(int depth) {
         this.depth = depth;
         return this;
     }
 
+    /**
+     * The overridden method of hash code computation.
+     * The formula for computation is:
+     * (width * PRIME_NUMBER + height) * PRIME_NUMBER + depth,
+     * where:
+     * <ul>
+     * <li>width, height, depth are fields of the current Dimensions object</li>
+     * <li>PRIME_NUMBER is constant.
+     * Please see the {@link ua.org.training.view.GlobalConstants} interface for values</li>
+     * </ul>
+     * @return          integer value of hash code;
+     */
     @Override
     public int hashCode(){
         return (width * GlobalConstants.PRIME_NUMBER + height) * GlobalConstants.PRIME_NUMBER + depth;
     }
 
+    /**
+     * The overridden method for representing dimensions information as a string.
+     * @return          String value of dimensions information in human readable format
+     */
     @Override
     public String toString(){
         StringBuffer stringBuffer = new StringBuffer(GlobalConstants.STRING_BUFFER_SIZE);
