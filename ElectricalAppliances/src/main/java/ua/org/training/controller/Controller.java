3ce6463be1520b60d71fa5e476300005c5164ef8
kissik <karayuziv@gmail.com>
2019-12-04 23:16:05 +0200

    Controller. Add get electrical appliance method

--- ElectricalAppliances/src/main/java/ua/org/training/controller/Controller.java
+++ ElectricalAppliances/src/main/java/ua/org/training/controller/Controller.java
@@ -2,6 +2,7 @@ package ua.org.training.controller;
 
 import ua.org.training.view.GlobalConstants;
 import ua.org.training.view.View;
+import ua.org.training.model.ElectricalAppliance;
 
 /**
  * class Controller
@@ -9,6 +10,8 @@ import ua.org.training.view.View;
  * @version     1.0, 12/4/2019
  */
 import java.util.ArrayList;
+
+import static java.lang.System.exit;
 
 public class Controller {
     private View view;
@@ -19,9 +22,23 @@ public class Controller {
 
     public void processUser() {
         UtilityController utilityController = new UtilityController();
-        ArrayList<String> array = utilityController.getLines(GlobalConstants.SOURCE_ELECTRICAL_APPLIANCE_FILE_NAME);
+        ArrayList<ElectricalAppliance> electricalAppliancesArrayList = new ArrayList<>();
+        ArrayList<String> array = utilityController
+                .getLines(GlobalConstants.SOURCE_ELECTRICAL_APPLIANCE_FILE_NAME);
         for(String string : array){
-            view.printMessage(string);
+            electricalAppliancesArrayList.add(getElectricalAppliance(string));
         }
+        for(ElectricalAppliance electricalAppliance : electricalAppliancesArrayList)
+            view.printMessage(electricalAppliance.toString());
+
+    }
+
+    private ElectricalAppliance getElectricalAppliance(String stringFromFile){
+        ElectricalAppliance electricalAppliance = new ElectricalAppliance();
+        if (stringFromFile.split(";").length != 3) exit(1);
+        electricalAppliance.setTitle(stringFromFile.split(";")[0]);
+        electricalAppliance.setCapacity(Integer.decode(stringFromFile.split(";")[1]));
+        electricalAppliance.setPlugIn(Boolean.valueOf(stringFromFile.split(";")[2]));
+        return electricalAppliance;
     }
 }
