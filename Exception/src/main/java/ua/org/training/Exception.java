55619ba0a04f4779c59ac5841de8501c129b9d09
kissik <karayuziv@gmail.com>
2019-12-07 23:29:22 +0200

    System.err.println() try-throw-catch sample

--- Exception/src/main/java/ua/org/training/Exception.java
+++ Exception/src/main/java/ua/org/training/Exception.java
@@ -3,12 +3,42 @@ package ua.org.training;
 public class Exception {
 
         public static void main(String[] args) {
-                // f(null);
+             /*   // f(null);
                 System.out.println("out".length() + " one more".length() + " string");
                 //System.out.println(sqr(2.0));
                 //System.out.println("result = " + sqr_nothing(0.));
                 System.out.println("area = " + area(-20, 30));
                 throw new Error();
+              */
+                System.err.println("#1.in");
+                f();
+                System.err.println("#1.out");
+        }
+
+        private static void f() {
+                System.err.println(".   #2.in");
+                g();
+                System.err.println(".   #2.out");
+        }
+
+        private static void g() {
+                System.err.println(".   .   #3.in");
+                try {
+                        h();
+                } catch (Error e) {
+                        System.err.println(".   .   #3.CATCH");
+                }
+                System.err.println(".   .   #3.out");
+        }
+
+        private static void h() {
+                System.err.println(".   .   .   #4.in");
+                if (true) {
+                        System.err.println(".   .   .   #4.RETURN");
+                        throw new Error();
+
+                }
+                System.err.println(".   .   .   #4.out");
         }
 
         private static void f(NullPointerException e) {
