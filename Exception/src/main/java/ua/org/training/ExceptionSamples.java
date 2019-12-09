8fc0a50cc15f170a7f356d8f5d2137fd2870afa4
kissik <karayuziv@gmail.com>
2019-12-09 14:43:44 +0200

    Nested try-catch-finally

--- Exception/src/main/java/ua/org/training/ExceptionSamples.java
+++ Exception/src/main/java/ua/org/training/ExceptionSamples.java
@@ -21,7 +21,91 @@ public class ExceptionSamples {
                         } catch (RuntimeException e) {
                         }
                  */
-                fg();
+                fk();
+        }
+
+        public static void fk(){
+                try {
+                        System.err.print(" 0");
+                        try {
+                                System.err.print(" 1");
+                                if (true) {throw new Error();}
+                                System.err.print(" 2");
+                        } catch (RuntimeException e) {
+                                System.err.print(" 3");
+                        } finally {
+                                System.err.print(" 4");
+                        }
+                        System.err.print(" 5");
+                } catch (Exception e) {
+                        System.err.print(" 6");
+                } finally {
+                        System.err.print(" 7");
+                }
+                System.err.print(" 8");
+        }
+
+        public static void fj(){
+                try {
+                        System.err.print(" 0");
+                        try {
+                                System.err.print(" 1");
+                                if (true) {throw new Exception();}
+                                System.err.print(" 2");
+                        } catch (RuntimeException e) {
+                                System.err.print(" 3");
+                        } finally {
+                                System.err.print(" 4");
+                        }
+                        System.err.print(" 5");
+                } catch (Exception e) {
+                        System.err.print(" 6");
+                } finally {
+                        System.err.print(" 7");
+                }
+                System.err.print(" 8");
+        }
+
+        public static void fi(){
+                try {
+                        System.err.print(" 0");
+                        try {
+                                System.err.print(" 1");
+                                if (true) {throw new RuntimeException();}
+                                System.err.print(" 2");
+                        } catch (RuntimeException e) {
+                                System.err.print(" 3");
+                        } finally {
+                                System.err.print(" 4");
+                        }
+                        System.err.print(" 5");
+                } catch (Exception e) {
+                        System.err.print(" 6");
+                } finally {
+                        System.err.print(" 7");
+                }
+                System.err.print(" 8");
+        }
+
+        public static void fh(){
+                try {
+                        System.err.print(" 0 ");
+                        try {
+                                System.err.print(" 1 ");
+                                // no more exception!
+                                System.err.print(" 2 ");
+                        } catch (RuntimeException e) {
+                                System.err.print(" 3 ");
+                        } finally {
+                                System.err.print(" 4 ");
+                        }
+                        System.err.print(" 5 ");
+                } catch (Exception e) {
+                        System.err.print(" 6 ");
+                } finally {
+                        System.err.print(" 7 ");
+                }
+                System.err.print(" 8 ");
         }
 
         public static void fg(){
